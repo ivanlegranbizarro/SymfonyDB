5a5c626f05ef72d58349ed6b30d9bf65ec762ce7
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-09-05 18:37:34 +0200

    setup controller products and tags

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Entity\Tag;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-  #[Route('/', name: 'app_home')]
-  public function home(): Response
+  public function __construct(private EntityManagerInterface $em)
   {
-    return $this->render('page/home.html.twig');
+  }
+  #[Route('/', name: 'app_home')]
+  public function home(Product $product): Response
+  {
+    $products = $this->em->getRepository(Product::class)->findAll($product);
+    return $this->render('page/home.html.twig', ['products' => $products]);
+  }
+
+  #[Route('/tag/{id}', name: 'app_tag')]
+  public function tag(Tag $tag): Response
+  {
+    return $this->render('page/tag.html.twig', ['tag' => $tag]);
   }
 }
